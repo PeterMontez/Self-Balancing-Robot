de5b05b06e10ac3df590f6957b63f912472cea0f
NycollasSobolevski <sobolevski.academy@gmail.com>
2023-06-15 16:54:30 -0300

    UPDATE: ERRINHO

--- HW-612/sensor_2.ino
+++ HW-612/sensor_2.ino
@@ -2,13 +2,10 @@ float gX, gx2,aX, ax2;
 
 void setup() {
   Serial.begin(9600);
-
   Serial.println("Porta serial ligada!");
-
   setupSensor();
 }
 
-int count;
 void loop() {
   Serial.println(String(getGyro()));
   Serial.println(String(getAccel()));
