dae7c277483f87e5c21b30fb2e693721b28c5e8f
NycollasSobolevski <sobolevski.academy@gmail.com>
2023-06-15 16:02:40 -0300

    UPDATE: Code

--- HW-612/sensor_2.ino
+++ HW-612/sensor_2.ino
@@ -1,59 +1,44 @@
-    /*
-  by MohammedDamirchi
-  Home
-*/
 #include <MPU9250_asukiaaa.h>
 #include <Adafruit_BMP280.h>
 
 Adafruit_BMP280 bme; // I2C
 MPU9250_asukiaaa mySensor;
-float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
+float aX, ax2;
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial);
+  Serial.begin(9600);
+
+  Serial.println("Porta serial ligada!");
 
   Wire.begin();
   mySensor.setWire(&Wire);
 
   bme.begin();
   mySensor.beginAccel();
-  mySensor.beginGyro();
-  mySensor.beginMag();
-
+  
   // You can set your own offset for mag values
   // mySensor.magXOffset = -50;
   // mySensor.magYOffset = -55;
   // mySensor.magZOffset = -10;
 }
-int maxy=0, miny=0;
+
+int count;
 void loop() {
-
-  if (mySensor.accelUpdate() == 0) {
-    aX = mySensor.accelX();
-    aY = mySensor.accelY();
-    aZ = mySensor.accelZ();
-    aSqrt = mySensor.accelSqrt();
-    Serial.print("accelX: " + String(aX));
-    Serial.print("\taccelY: " + String(aY));
-    Serial.print("\taccelZ: " + String(aZ));
-    Serial.print("\taccelSqrt: " + String(aSqrt));
+  if(count < 5){
+    if (mySensor.accelUpdate() == 0)
+    {
+      aX = mySensor.accelX();
+      ax2 += aX * 100;
+//      Serial.print(" AclXXX: " + String(aX * 100));
+//      Serial.println("");   
+    }  
   }
-
-  if (mySensor.gyroUpdate() == 0) {
-    gX = mySensor.gyroX();
-    gY = mySensor.gyroY();
-    gZ = mySensor.gyroZ();
-    Serial.print("\tgyroX: " + String(gX));
-    Serial.print("\tgyroY: " + String(gY));
-    Serial.print("\tgyroZ: " + String(gZ));
+  else{
+    ax2 /= count;
+    Serial.println("Mean acl: " + String(ax2));
+    count = 0;
   }
-  if(maxy < gY)
-    maxy = gY;
-  if(miny > gY)
-    miny = gY;
-  Serial.println(""); // Add an empty line
-  Serial.println("min Y - " + String(miny) + "max y - " + String(maxy));
-  }
-
   
+  count++;
+  
+}
