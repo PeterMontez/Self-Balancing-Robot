6424d73db111bc473abfb4ea98530b043efb04e3
Nycollas Sobolevski <112396150+NycollasSobolevski@users.noreply.github.com>
2023-06-16 15:36:35 -0300

    Update sensor_2.ino

--- HW-612/sensor_2.ino
+++ HW-612/sensor_2.ino
@@ -1,15 +1,16 @@
 #include <MPU9250_asukiaaa.h>
+#include <Adafruit_BMP280.h>
 
-float gX, gx2,aX, ax2;
+float gX, gx2, aX, ax2;
 MPU9250_asukiaaa mySensor;
+Adafruit_BMP280 bme;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Porta serial ligada!");
   setupSensor();
 }
 
 void loop() {
   Serial.println(String(getGyro()));
-  Serial.println(String(getAccel()));
+  //  Serial.println(String(getAccel()));
 }
